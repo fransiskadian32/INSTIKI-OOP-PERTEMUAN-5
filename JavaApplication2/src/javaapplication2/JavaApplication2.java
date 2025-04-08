c11cc29003bdaf763b4ca8ad70ccd21ebe3f991c
PC-F-22 <email akun Github>
2025-04-08 21:44:35 +0800

    Keterangan commit

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -14,7 +14,28 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        jajan pancong = new jajan ();
+        //pancong.bentuk = "lonjong";
+        pancong.setBentuk("lonjong");
+        pancong.rasa = "Tiramisu";
+        pancong.harga = 5000;
+        pancong.warna = "pink";
+        
+        System.out.println("Bentuknya: "+pancong.getBentuk());
     }
     
 }
+
+ class jajan {
+    private String bentuk;
+    String warna;
+    float harga;
+    String rasa;
+    
+    public void setBentuk(String bnt) {
+        this.bentuk = bnt;
+    }
+     public String getBentuk(){
+        return this.bentuk;
+}
+ }
